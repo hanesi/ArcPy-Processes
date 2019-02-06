b11b83c8387aa60d24299a11e44f13a930a96169
hanesi <ian_hanes@utexas.edu>
2019-02-06 09:17:11 -0500

    Update FeatureCountWithSubfolders.py

--- FeatureCountWithSubfolders.py
+++ FeatureCountWithSubfolders.py
@@ -1,17 +1,28 @@
+"""
+This script counts the number of features in a shape files
+with multiple subfolders. It returns the parent folder and
+the number of features.
+"""
 import os
 import arcpy
 
-directory = r"X:\Projects_2017\Corelogic Parcel\Supplementary Data Attributes\pyTest"
+directory = r"FILE PATH"
+#Set parent directory
 for root, dirs, files2 in os.walk(directory):
+    #Walk through sub folders in parent directory
     for dir in dirs:
             for root, dirs, files in os.walk(directory + "/" + dir):
                 ct = 0
                 for file in files:
                     if file.endswith(".shp"):
+                        #Construct file path
                         pth = os.path.join(directory + "/" + dir,file)
+                        #Retrieve file name (no extension, a requirement of the function)
                         filename = file.replace(".shp","")
+                        #Create in memory copy of file and count the features
                         arcpy.MakeFeatureLayer_management(pth,filename)
                         result = arcpy.GetCount_management(file)
                         count = int(result.getOutput(0))
                         ct += count
-            print(ct)
+            print(dir + ": " + str(ct))
+            
