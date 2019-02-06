022618bdbacc1d0b3b3b4f0cf3eca2938572ff5a
hanesi <ian_hanes@utexas.edu>
2019-02-06 09:11:33 -0500

    Update CountFeatures.py

--- CountFeatures.py
+++ CountFeatures.py
@@ -1,13 +1,21 @@
+"""
+This script counts the number of features in a single folder
+containing multiple shapefiles
+"""
 import os
 import arcpy
 
-directory = r"C:\Users\hanesi\Desktop\Projects\Python\10"
+#Input the parent file path
+directory = r"FILE PATH"
 for root, dirs, files in os.walk(directory):
     ct = 0
     for file in files:
         if file.endswith(".shp"):
+            #Set the path of each individual file
             pth = os.path.join(directory,file)
+            #Retrieve the filename
             filename = file.replace(".shp","")
+            #Create an in memory copy of each file and count the number of features
             arcpy.MakeFeatureLayer_management(pth,filename)
             result = arcpy.GetCount_management(file)
             count = int(result.getOutput(0))
